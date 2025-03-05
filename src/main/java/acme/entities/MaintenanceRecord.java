3068f26644bfe55edb7ea1efeb271f6668934ec8
MIREGAR1 <migreggar@alum.us.es>
2025-03-05 20:48:42 +0100

    feat(MaintenanceRecord): Añadida la entidad para modelar los MaintenanceRecords
    Se ha añadido todas las propiedades y asociaciones de las
    MaintenanceRecords junto con sus validaciones pertinentes
    
    REFS #32

--- src/main/java/acme/entities/MaintenanceRecord.java
+++ src/main/java/acme/entities/MaintenanceRecord.java
@@ -1,6 +1,59 @@
+
 package acme.entities;
 
+import java.util.Date;
 
-public class MaintenanceRecord {
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
 
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class MaintenanceRecord extends AbstractEntity {
+	// Serialisation version -------------------------------------------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes ------------------------------------------------------------------------------------------------
+	@Mandatory
+	@ValidMoment
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				moment;
+
+	@Mandatory
+	@ValidString(pattern = "^(PENDING|IN PROGRESS|COMPLETED)$")
+	private String				currentStatus;
+
+	@Mandatory
+	@ValidMoment
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				inspectionDueDate;
+
+	@Mandatory
+	@ValidNumber(min = 0)
+	private Integer				estimatedCost;
+
+	@Optional
+	@ValidString(max = 255)
+	private String				notes;
+
+	@Mandatory
+	private Boolean				published;
+
+	@Mandatory
+	@Valid
+	@ManyToOne
+	private Aircraft			aircraft;
 }
