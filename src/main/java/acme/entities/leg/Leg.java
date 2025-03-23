c8b03c5ac680d3282961c3630275b78b37f6e706
Adolfo Borrego <adolfo.bglez@gmail.com>
2025-03-23 18:32:02 +0100

    Arreglo entidades y populate sample de datos stundent1

--- src/main/java/acme/entities/leg/Leg.java
+++ src/main/java/acme/entities/leg/Leg.java
@@ -1,20 +1,22 @@
 
 package acme.entities.leg;
 
+import java.time.Duration;
 import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 import javax.validation.Valid;
-import javax.validation.constraints.DecimalMin;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidString;
+import acme.client.helpers.MomentHelper;
 import acme.entities.aircraft.Aircraft;
 import acme.entities.airport.Airport;
 import acme.entities.flight.Flight;
@@ -43,37 +45,50 @@ public class Leg extends AbstractEntity {
 	private Date				scheduledArrival;
 
 	@Mandatory
-	@DecimalMin("0.0")
-	@Automapped
-	private Double				hours;
-
-	@Mandatory
 	@ValidString(pattern = "^(ON-TIME|DELAYED|CANCELLED|LANDED)$")
 	@Automapped
 	private String				status;
 
 	// Derived attributes -----------------------------------------------------
 
+
+	@Transient
+	private Double getDuration() {
+		Duration duration = MomentHelper.computeDuration(this.getScheduledDeparture(), this.getScheduledArrival());
+
+		return duration.getSeconds() / 60.;
+	}
+
+	@Transient
+	private String getFlightNumber() {
+		String iataCode = this.aircraft.getAirline().getIataCode();
+
+		String suffix = String.format("%04d", this.getId() % 10000);
+
+		return iataCode + suffix;
+	}
+
 	// Relationships ----------------------------------------------------------
+
 
 	@Mandatory
 	@ManyToOne
 	@Valid
-	private Airport				departureAirport;
+	private Airport		departureAirport;
 
 	@Optional
 	@ManyToOne
 	@Valid
-	private Airport				arrivalAirport;
+	private Airport		arrivalAirport;
 
 	@Optional
 	@ManyToOne
 	@Valid
-	private Aircraft			aircraft;
+	private Aircraft	aircraft;
 
 	@Optional
 	@ManyToOne
 	@Valid
-	private Flight				flight;
+	private Flight		flight;
 
 }
