dcbb9fd5b92f83e3dd6cb8d22a05bb3f79405eb2
miguelmir123 <miguelmirceballos@gmail.com>
2025-03-28 16:15:16 +0100

    feat: Service Body And Methods Added
    The methods are not working good yet, it is just the interface methods that were added

--- src/main/java/acme/features/flightCrewMember/flightAssignment/FlightAssignmentCreateService.java
+++ src/main/java/acme/features/flightCrewMember/flightAssignment/FlightAssignmentCreateService.java
@@ -1,9 +1,38 @@
 package acme.features.flightCrewMember.flightAssignment;
 
+import java.util.Collection;
+
 import acme.entities.flightAssignment.FlightAssignment;
 import acme.realms.flightCrewMember.FlightCrewMember;
 
 @GuiService
 public class FlightAssignmentCreateService  extends AbstractGuiService<FlightCrewMember, FlightAssignment>  {
     
+    // Internal state ---------------------------------------------------------
+
+	@Autowired
+	private FlightAssignmentRepository repository;
+
+	// AbstractGuiService interface -------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		
+
+		//super.getBuffer().addData();
+	}
+
+	@Override
+	public void unbind(final FlightAssignment flightAssignment) {
+		Dataset dataset;
+		dataset = super.unbindObject(flightAssignment, "duty", "momentOfLastUpdate", "currentStatus");
+
+		super.getResponse().addData(dataset);
+	}
 }
--- src/main/java/acme/features/flightCrewMember/flightAssignment/FlightAssignmentDeleteService.java
+++ src/main/java/acme/features/flightCrewMember/flightAssignment/FlightAssignmentDeleteService.java
@@ -1,9 +1,42 @@
 package acme.features.flightCrewMember.flightAssignment;
 
+import java.util.Collection;
+
 import acme.entities.flightAssignment.FlightAssignment;
 import acme.realms.flightCrewMember.FlightCrewMember;
 
 @GuiService
 public class FlightAssignmentDeleteService  extends AbstractGuiService<FlightCrewMember, FlightAssignment> {
+
+    // Internal state ---------------------------------------------------------
+
+	@Autowired
+	private FlightAssignmentRepository repository;
+
+	// AbstractGuiService interface -------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Collection<FlightAssignment> flightAssignments;
+
+		Integer memberId = super.getRequest().getPrincipal().getActiveRealm().getId();
+		flightAssignments = this.repository.findAllMyPlannedFlightAssignments(memberId);
+
+		super.getBuffer().addData(flightAssignments);
+	}
+
+	@Override
+	public void unbind(final FlightAssignment flightAssignment) {
+		Dataset dataset;
+		dataset = super.unbindObject(flightAssignment, "duty", "momentOfLastUpdate", "currentStatus");
+
+		super.getResponse().addData(dataset);
+	}
     
 }
