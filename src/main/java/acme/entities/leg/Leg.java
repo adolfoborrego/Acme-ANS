66f9fe16de4a5da1e4678424ad62df9c47b2d72e
Adolfo Borrego <adolfo.bglez@gmail.com>
2025-05-26 01:20:51 +0200

    indices comentados para testing

--- src/main/java/acme/entities/leg/Leg.java
+++ src/main/java/acme/entities/leg/Leg.java
@@ -27,6 +27,10 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+// @Table(name = "leg", indexes = {
+//	@Index(name = "idx_leg_flight", columnList = "flight_id"), @Index(name = "idx_leg_code", columnList = "code"), @Index(name = "idx_leg_scheduled_arrival", columnList = "scheduledArrival"), @Index(name = "idx_leg_published", columnList = "published"),
+//	@Index(name = "idx_leg_flight_departure", columnList = "flight_id, scheduledDeparture")
+// })
 public class Leg extends AbstractEntity {
 
 	// Serialisation version --------------------------------------------------
